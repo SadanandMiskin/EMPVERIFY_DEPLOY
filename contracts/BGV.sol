d8eb83b943d04ad41bc9fa46a0c4ee26516b4283
kuldeepkumar2212 <kuldeepkumarraju2001@gmail.com>
2024-04-09 15:29:34 +0530

    feat: verifier registration in contract

--- contracts/BGV.sol
+++ contracts/BGV.sol
@@ -29,6 +29,12 @@ contract BGV {
         bool approvedByGovernment;
     }
 
+        // Struct to represent a verifier
+    struct Verifier {
+        string organizationName;
+        address verifierAddress;
+    }
+
     // Mapping to store students by their Ethereum address
     mapping(address => Student) public students;
 
@@ -38,11 +44,17 @@ contract BGV {
     // Mapping to store license numbers to prevent duplicates
     mapping(string => bool) public licenseNumbers;
 
+     // Mapping to store verifiers by their Ethereum address
+    mapping(address => Verifier) public verifiers;
+
     // Array to store university addresses
     address[] public universityAddresses;
 
+    // Array to store verifier addresses
+    address[] public verifierAddresses;
+
     // Event to emit when a new student is added
-    // event StudentAdded(address indexed studentAddress, string name, address indexed addedBy);
+    event StudentAdded(address indexed studentAddress, string name, address indexed addedBy);
 
     // Event to emit when a new document is added to a student
     event DocumentAdded(address indexed studentAddress, string documentName, string hashValue, AccessType access);
@@ -53,6 +65,14 @@ contract BGV {
     // Event to emit when document access type is changed
     event DocumentAccessChanged(address indexed studentAddress, string hashValue, AccessType newAccessType);
 
+     // Event to emit when a new verifier is added
+    event VerifierAdded(address indexed verifierAddress, string organizationName);
+
+    // Modifier to ensure only a legitimate verifier can add themselves
+    modifier onlyLegitimateVerifier(address _organizationAddress) {
+        require(verifiers[msg.sender].verifierAddress == address(0), "Verifier already exists");
+        _;
+    }
     // Modifier to ensure only a legitimate university can add a student
     modifier onlyLegitimateUniversity(address _universityAddress) {
         require(universities[_universityAddress].approvedByGovernment && bytes(universities[_universityAddress].licenseNumber).length > 0, "Only a legitimate university can add a student");
@@ -60,11 +80,7 @@ contract BGV {
     }
 
     // Function to add a new student
-    // mapping(address => Student) public students;
-    address[] public studentAddresses; // Maintain a list of student addresses
-
-    // Function to add a new student
-    function addStudent(string memory _name, address _studentAddress) public {
+    function addStudent(string memory _name, address _studentAddress) public onlyLegitimateUniversity(msg.sender) {
         // Ensure the student does not already exist
         require(students[_studentAddress].studentAddress == address(0), "Student already exists");
 
@@ -74,31 +90,9 @@ contract BGV {
         newStudent.studentAddress = _studentAddress;
         newStudent.addedBy = msg.sender; // Set the university that added the student
 
-        // Add the student address to the list
-        studentAddresses.push(_studentAddress);
-
         // Emit an event to notify that a new student has been added
         emit StudentAdded(_studentAddress, _name, msg.sender);
     }
-
-    // Function to get all students' names
-    function getAllStudentNames() public view returns (string[] memory) {
-        string[] memory names = new string[](studentAddresses.length);
-
-        for (uint256 i = 0; i < studentAddresses.length; i++) {
-            names[i] = students[studentAddresses[i]].name;
-        }
-
-        return names;
-    }
-
-    // Function to get all students' addresses
-    function getAllStudentAddresses() public view returns (address[] memory) {
-        return studentAddresses;
-    }
-
-    // Event to emit when a new student is added
-    event StudentAdded(address indexed studentAddress, string name, address indexed addedBy);
 
     // Function to add a document to an existing student
     function addDocumentToStudent(address _studentAddress, string memory _documentName, string memory _hashValue, AccessType _access) public {
@@ -197,23 +191,19 @@ contract BGV {
         return allUniversities;
     }
 
-    // mapping(address => Student) public students;
+     // Function to add a new verifier
+    function addVerifier(string memory _organizationName) public onlyLegitimateVerifier(msg.sender) {
+        
+        require(verifiers[msg.sender].verifierAddress == address(0), "Verifier with this address already exists");
+        // Create a new verifier
+        Verifier storage newVerifier = verifiers[msg.sender];
+        newVerifier.organizationName = _organizationName;
+        newVerifier.verifierAddress = msg.sender;
 
-    // // Function to get all students
-    // function getAllStudents() public view returns (Student[] memory) {
-    //     // Create an array to store details of all students
-    //     Student[] memory allStudents = new Student[](addressCount);
+        // Add verifier address to the array
+        verifierAddresses.push(msg.sender);
 
-    //     // Iterate over all student addresses
-    //     for (uint256 i = 0; i < addressCount; i++) {
-    //         // Get the student details
-    //         Student memory student = students[studentAddresses[i]];
-
-    //         // Store the student details in the array
-    //         allStudents[i] = student;
-    //     }
-
-    //     // Return the array containing details of all students
-    //     return allStudents;
-    // }
+        // Emit an event to notify that a new verifier has been added
+        emit VerifierAdded(msg.sender, _organizationName);
+    } 
 }
