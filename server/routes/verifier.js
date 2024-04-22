1ef4c864c5d220da275ae6bd5617f107f9646db5
Sadanand Miskin <miskinsadanand@gmail.com>
2024-04-22 11:44:14 +0530

    merged few html files to ejs  - admin,verf login

--- server/routes/verifier.js
+++ server/routes/verifier.js
@@ -23,7 +23,11 @@ router.get('/verifierHome', isVerifier, async (req, res) => {
         
         console.log(studentList)
         if(studentList == null) {
+<<<<<<< Updated upstream
             return res.render('verifier-search')
+=======
+            return res.render('verifier-search', {verifier: verifier.name})
+>>>>>>> Stashed changes
         }
         else {
             
